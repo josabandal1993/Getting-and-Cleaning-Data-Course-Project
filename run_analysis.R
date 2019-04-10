63dba40ea537c006d8f2918fc8b6ad9380d9e46f
josabandal1993 <jake.sabandal@eee.upd.edu.ph>
2019-04-10 18:47:43 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,23 +4,21 @@ train_x = read.table("UCIHARDataset/train/X_train.txt")
 train_y = read.table("UCIHARDataset/train/y_train.txt")
 train_sub = read.table("UCIHARDataset/train/subject_train.txt")
 train = c("train") #Creating a category column for training dataset
-train_data = cbind(train,train_sub,train_y,train_x)
+train_data = cbind(train_sub,train_y,train_x)
 
 #Reading the test dataset
 test_x = read.table("UCIHARDataset/test/X_test.txt")
 test_y = read.table("UCIHARDataset/test/y_test.txt")
 test_sub = read.table("UCIHARDataset/test/subject_test.txt")
 test = c("test") #Creating a category column for test dataset
-test_data = cbind(test,test_sub,test_y,test_x)
+test_data = cbind(test_sub,test_y,test_x)
 
 #Merging train and test dataset
 ##Changing the column names for smooth merging
-names(train_data)[1] <- "dataset"
-names(test_data)[1] <- "dataset"
-names(train_data)[2] <- "subject"
-names(test_data)[2] <- "subject"
-names(train_data)[3] <- "activity"
-names(test_data)[3] <- "activity"
+names(train_data)[1] <- "subject"
+names(test_data)[1] <- "subject"
+names(train_data)[2] <- "activity"
+names(test_data)[2] <- "activity"
 
 ##Combine the test and train dataset using rbind since they contain same set of columns
 
@@ -35,7 +33,7 @@ loc <- sort(c(mean,std)) #Now we have the location of the features that contains
 
 #This portion saves only the features with mean and standard deviation note that we added "3"
 #in loc to compensate for the subject,dataset, activity variables; thus completes number 2
-filtereddat <- dat[,c(1:3,loc+3)]
+filtereddat <- dat[,c(1:2,loc+2)]
 #Number 2
 print(filtereddat)
 
@@ -54,7 +52,7 @@ print(filtereddat)
 
 #For problem 4, we need to use the feature labels to rename the variables in "filtereddat"
 varnames = features[loc,2]
-names(filtereddat)[4:(length(varnames)+3)] <- as.character(varnames) #Thus this completes our solution for number 4
+names(filtereddat)[3:(length(varnames)+2)] <- as.character(varnames) #Thus this completes our solution for number 4
 #number 4
 print(filtereddat) #Tidy data set for numbers, 1-4
 
@@ -62,13 +60,12 @@ print(filtereddat) #Tidy data set for numbers, 1-4
 aggregatedmean = filtereddat
 #Aggregate the filtereddat by subject participant followed by
 #their corresponding activity
-aggregatedmean = aggregate(aggregatedmean[4:82], by=list(aggregatedmean$dataset,aggregatedmean$subject,aggregatedmean$activity),
+aggregatedmean = aggregate(aggregatedmean[3:81], by=list(aggregatedmean$subject,aggregatedmean$activity),
                            FUN=mean, na.rm = TRUE)
-names(aggregatedmean)[1] <- "dataset"
-names(aggregatedmean)[2] <- "subject"
-names(aggregatedmean)[3] <- "activity"
+names(aggregatedmean)[1] <- "subject"
+names(aggregatedmean)[2] <- "activity"
 
 #Number 5
 print(aggregatedmean) #Tidy data set for number 5
 
-aggregated.txt = write.table(aggregatedmean, file = "newdataset.txt" ,row.names = FALSE)
+aggregated.txt = write.table(aggregatedmean, file = "newdataset.txt" ,row.names = FALSE, col.names = TRUE)
